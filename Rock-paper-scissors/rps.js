2f40a13712fa1e4419350e3e6acccdec173d5007
Etoilaria <105894701+Etoilaria@users.noreply.github.com>
2023-06-06 23:55:27 -0400

    FIRST UPDATE(Proper)
    I forgot to save in the last one, lol. This one has the actual code

---
+++ Rock-paper-scissors/rps.js
@@ -0,0 +1,8 @@
+let compChoice = {Value: ""};
+let playerChoice;
+let compChoiceInt = 0;
+let playerChoiceInt = 0;
+const buttons = document.querySelectorAll('.btn');
+
+let playerScore = 0;
+let compScore = 0;
